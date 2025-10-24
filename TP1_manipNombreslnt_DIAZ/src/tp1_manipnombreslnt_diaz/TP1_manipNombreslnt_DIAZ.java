de8b0262abda52857fe246fe51ce7f61ecf362e2
user <user@10.3.62.76>
2025-10-24 12:01:14 +0200

    ajout TD1 manipNombresInt

--- TP1_manipNombreslnt_DIAZ/src/tp1_manipnombreslnt_diaz/TP1_manipNombreslnt_DIAZ.java
+++ TP1_manipNombreslnt_DIAZ/src/tp1_manipnombreslnt_diaz/TP1_manipNombreslnt_DIAZ.java
@@ -4,9 +4,11 @@
  */
 package tp1_manipnombreslnt_diaz;
 
-/**
- *
- * @author user
+import java.util.Scanner;
+
+/** Saisie et manipulation de nombres 
+ * date : 24 octobre 2025
+ * @author lena 
  */
 public class TP1_manipNombreslnt_DIAZ {
 
@@ -14,7 +16,18 @@ public class TP1_manipNombreslnt_DIAZ {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Entrez le premier nombre : ");
+        int nb1 = sc.nextInt();
+        System.out.print("Entrez le deuxième nombre : ");
+        int nb2 = sc.nextInt();
+
+        System.out.println("Somme : " + (nb1 + nb2));
+        System.out.println("Différence : " + (nb1 - nb2));
+        System.out.println("Produit : " + (nb1 * nb2));
+        System.out.println("Quotient entier : " + (nb1 / nb2));
+        System.out.println("Reste de la division : " + (nb1 % nb2));
     }
-    
+
 }
