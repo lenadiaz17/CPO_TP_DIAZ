eef42b0629b15592f0fc258179a2965f9539143a
user <user@10.3.62.76>
2025-12-08 15:30:16 +0100

    ajout mini projet version1

---
+++ miniprojet_cadenas_DIAZ/src/miniprojet_cadenas_diaz/Miniprojet_cadenas_DIAZ.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package miniprojet_cadenas_diaz;
+
+/**
+ *
+ * @author user
+ */
+public class Miniprojet_cadenas_DIAZ {
+
+   
+    public static void main(String[] args) {
+        new interface_jeu().setVisible(true);
+    }
+}
